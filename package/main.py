88e7d57e3fe2b03c02225e613689f3d3a928e494
airjason13 <airjason13@gmail.com>
2023-10-05 21:10:54 +0800

    1. test web UI

--- package/main.py
+++ package/main.py
@@ -5,6 +5,7 @@ from flask import Blueprint, render_template, url_for, redirect, request, sessio
 from flask_login import login_required, current_user, logout_user
 from . import db
 from . import auth
+import json
 main = Blueprint('main', __name__)
 
 
@@ -18,24 +19,45 @@ def index():
 @login_required
 def profile():
 	print("profile")
-	return render_template('profile.html', name=current_user.name, show_jpg_filename="CyberSecurity.jpg")
+	jpg_filename = "CyberSecurity.jpg"
+	try:
+		messages = request.args['messages']
+		# messages = session['messages']
+		print(messages)
+		filename = messages.strip('{}').split(':')[1]
+		print("filename :", filename)
+		filename = filename.replace('"', '')
+		filename = filename.lstrip(" ")
+		print("filename :", filename)
+		jpg_filename = filename
+	except Exception as e:
+		print(e)
+	print("jpg_filename :", jpg_filename)
+	return render_template('profile.html', name=current_user.name, show_jpg_filename=jpg_filename)
 
 
-@main.route('/uploads/', methods=['POST'])
+@main.route('/uploads/', methods=['GET', 'POST'])
 @login_required
 def uploads():
 	print("request.files['file']", request.files['file'])
 	f = request.files['file']
-	UPLOAD_FOLDER = os.getcwd() + "/data/"
+	UPLOAD_FOLDER = os.getcwd() + "/package/static/material/"
 	f.save(UPLOAD_FOLDER + "/" + f.filename)
+	print("f.filename:", f.filename)
+	os.sync()
 
-	return render_template('profile.html', name=current_user.name, show_jpg_filename=f.filename)
+	messages = json.dumps({"name": f.filename})
+	session['messages'] = messages
+	return redirect(url_for('main.profile', messages=messages))
+	# return render_template('profile.html', name=current_user.name, show_jpg_filename="CyberSecurity.jpg")
+	# return render_template('profile.html', name=current_user.name, show_jpg_filename="CyberSecurity.jpg")
 
 
 @main.route('/get_thumbnail/<filename>')
 @login_required
 def route_get_thumbnail(filename):
 	print("os.getcwd :", os.getcwd())
+	print("filename:", filename)
 	material_path = os.getcwd() + "/package/static/material/"
 	return send_from_directory(material_path, filename, as_attachment=True)
 
@@ -51,11 +73,6 @@ def reauth():
 		return render_template('profile.html', name="Bad guy!")
 	return render_template('profile.html', name=current_user.name, show_jpg_filename="CyberSecurity.jpg")
 
-
-@main.route("/helloworld")
-def hello_world():
-    return "<p>Hello, World from \
-                redirected page.!</p>"
 
 @main.before_app_request
 def create_tables():
